5e6b954383d652d2cab18cede85be70fecec01ce
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-11-27 12:11:31 +0530

    Create alphacheck.c

---
+++ alphacheck.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    scanf("%c",&c)
+    if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+    printf("Alphabet");
+    else
+    printf("Not an Alphabet");
+
+    return 0;
+}
