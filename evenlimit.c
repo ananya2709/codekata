e71a97fccd89f47c686aa8dccd3c230d0001fb16
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-11-29 11:25:22 +0530

    Create evenlimit.c

---
+++ evenlimit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int  a,b,i;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2==0)
+        {   
+            printf("%d\t",i);
+            
+        }
+    }
+    
+    
+
+    return 0;
+}
