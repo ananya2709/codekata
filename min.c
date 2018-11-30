dea44c4d21a7b91554b491e13d7acb3b3def9b70
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-11-30 11:30:50 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[10],min,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+printf("%d",min);
+
+    return 0;
+}
