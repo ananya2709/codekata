71bd8980e117cc8ef6e1e3e8f502e31bb36bceb4
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-12-07 02:50:21 -0800

    Create primecount.c

---
+++ primecount.c
@@ -0,0 +1,23 @@
+int main()
+{
+int count=0,a,b,i,j,prime=0;
+scanf("%d%d",&a,&b);
+for(i=a;i<b-1;i++)
+{
+    for(j=2;j<i/2;j++)
+    {
+        if(i%j==0)
+        {
+            prime=1;
+            break;
+        }
+    }if(prime==0)
+    {
+        count++;
+    }
+}
+printf("%d",count);
+    return 0;
+}
+
+         
