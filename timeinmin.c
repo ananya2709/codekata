eefd4cb3297f4945e82dd14b3496348be12a0fed
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-12-11 23:42:25 -0800

    Create timeinmin.c

---
+++ timeinmin.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int m,h,m1;
+    scanf("%d",&m);
+    if(m<60)
+    {
+        printf("%d",m);
+    }
+    else if(m==60)
+    {  
+        h=1;
+        m1=0;
+        printf("%d %d",h,m1);
+    }
+    else
+    {
+        h=m/60;
+        m1=m%60;
+        printf("%d %d",h,m1);
+    }
+    return 0;
+}
