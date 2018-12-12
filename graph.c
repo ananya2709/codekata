f61e026df99c02c4265c0095ebf92a715b57a428
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-12-11 23:57:46 -0800

    Update graph.c

--- graph.c
+++ graph.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    int x1,x2,y1,y2;
-    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
-    if(x1==x2||y1==y2)
+    int x1,x2,y1,y2,x3,y3;
+    scanf("%d%d%d%d%d%d",&x1,&y1,&x2,&y2,x3,ye);
+    if((x1==x2&&x1==x3)||(y1==y2&&y1==y3))
     {
         printf("yes");
         
