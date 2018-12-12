2b7d2050149865c19e3dd7d199d731d6e8f390d4
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2018-12-11 23:37:16 -0800

    Update timesub.c

--- timesub.c
+++ timesub.c
@@ -6,7 +6,13 @@ int main()
     scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
     if(m1<60&&m2<60)
     {
-        if(h1>h2)
+        if(h1==h2&&m1==m2)
+        {
+            h3=0;
+            m3=0;
+        }
+        
+        else if(h1>h2)
         {
             if(m1>m2)
             {
