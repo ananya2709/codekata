bc26a0b9082f842934c0c01b52eb6338025d95f9
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2019-05-24 12:52:39 +0530

    Update powernk.c

--- powernk.c
+++ powernk.c
@@ -3,15 +3,14 @@ void main()
 {
     int a,b,i,d=1;
     scanf("%d%d",&a,&b);
-    for(i=1;i<5;i++)
-    {
-        if(b^i==a)
+   
+        if(a%b==0)
         {
             printf("yes");
             d=0;
-            break;
+           
         }
-    }
+    
     if(d!=0)
     {
         printf("no");
