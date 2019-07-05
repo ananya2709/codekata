1789da2dff9c814c8233bf5e558abac4802b4281
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2019-07-05 20:10:13 +0530

    Create stringconcat.java

---
+++ stringconcat.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String s,a;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		a=sc.nextLine();
+		System.out.println(s+" "+a);
+	}
+}
