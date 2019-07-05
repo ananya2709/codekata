a99d49c7fb6c190419a9928907054c51232d0a08
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2019-07-05 19:59:45 +0530

    Create stringcompare.java

---
+++ stringcompare.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String s,a;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		a=sc.nextLine();
+		int l1,l2;
+		l1=s.length();
+		l2=a.length();
+		if(l1>l2)
+		System.out.println(s);
+		else if(l1<l2)
+		System.out.println(a);
+		else
+		System.out.println(a);
+		}
+		
+	
+}
